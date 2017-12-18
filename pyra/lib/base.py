4230e8cccc2997bd6bb7057c8a6a586cb8db99a0
jameXPS13PyCharm <wjcauskew@hotmail.com>
2017-12-18 15:51:29 +1100

    config info can be retrieved from the DB

--- pyra/lib/base.py
+++ pyra/lib/base.py
@@ -8,22 +8,24 @@ class BaseController():
         #set up the protocol
         self.request=request
         self.response=request.response
-        #set up c
+        #set up c,those are directly retrieved fro the DB
         c.site_name = config['site_name']
-        c.feedback_email = "S4M@unimelb.edu.au"
-        c.stemformatics_version = 1.0
-        c.hostname = "S4M_Host_Name"
-        c.json_tutorials_for_page="json_tute"
-        c.role="user"
-        c.production = "true"
-        c.debug = None
-        c.title = "S4M_title"
-        c.header = ""
+        c.feedback_email = config['feedback_email']
+        c.production = config['production']
+        c.stemformatics_version = config['stemformatics_version']
+        c.title = config['site_name']
+        # c attributes those are not from the DB
         c.user = ""
         c.uid = 0
         c.full_name = ""
         c.notifycation = ""
         c.header_selected = "contents"
+        c.hostname = "S4M_Host_Name"
+        c.json_tutorials_for_page = "json_tute"
+        c.role="user"
+        c.debug = None
+        c.header = ""
+
         # set up h(Note that the site url is hard coded)
         # site url for VM1:'https://www-pyramid1.stemformatics.org'
         self.helper = h(self.request,'https://www-pyramid1.stemformatics.org')
