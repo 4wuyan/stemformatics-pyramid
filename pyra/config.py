4e13a634c10562223c65c5d1504a099dd3451539
jameXPS13PyCharm <wjcauskew@hotmail.com>
2017-12-18 15:38:25 +1100

    config test 1.1

--- pyra/config.py
+++ pyra/config.py
@@ -4,6 +4,7 @@ import psycopg2.extras
 config = {
  'psycopg2_conn_string':"host='localhost' dbname='portal_beta' user='portaladmin'"
 }
+
 conn_string = config['psycopg2_conn_string']
 conn = psycopg2.connect(conn_string)
 cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
@@ -17,7 +18,6 @@ result_dir={}
 for row in result:
     ref_type = row['ref_type']
     ref_id = row['ref_id']
-
     try:
        ref_id = int(ref_id)
     except:
@@ -26,4 +26,4 @@ for row in result:
     result_dir[ref_type] = ref_id
 
 for key in result_dir:
- config[key] = result[key]
+ config[key] = result_dir[key]
