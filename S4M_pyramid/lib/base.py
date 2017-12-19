4f1e2008db451fa5fd8cee909e59282d790c6ee7
jameXPS13PyCharm <wjcauskew@hotmail.com>
2017-12-19 14:58:00 +1100

    trying to get tutorial pages

--- S4M_pyramid/lib/base.py
+++ S4M_pyramid/lib/base.py
@@ -1,6 +1,8 @@
 from S4M_pyramid.lib.empty_class import EmptyClass as c
 from S4M_pyramid.lib.helper import Helper as h
 from S4M_pyramid.config import *
+from S4M_pyramid.model.stemformatics.stemformatics_help import *
+import json
 
 class BaseController():
 
@@ -26,6 +28,11 @@ class BaseController():
         c.debug = None
         c.header = ""
         c.breadcrumbs = []
+
+        #set tutorial page
+        c.tutorials_for_page = Stemformatics_Help.get_help_for_page("contents/contact_us",request.params)
+        c.json_tutorials_for_page =  json.dumps(c.tutorials_for_page)
+        print(c.tutorials_for_page)
 
         # set up h(Note that the site url is hard coded)
         # site url for VM1:'https://www-pyramid1.stemformatics.org'
