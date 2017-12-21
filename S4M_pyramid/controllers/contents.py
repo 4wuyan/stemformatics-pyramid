3d1c8c20d531c9e9c801b05eb35c83dba9f041e8
jameXPS13PyCharm <wjcauskew@hotmail.com>
2017-12-21 14:33:19 +1100

    more changes

--- S4M_pyramid/controllers/contents.py
+++ S4M_pyramid/controllers/contents.py
@@ -18,34 +18,34 @@ class ContentsController(BaseController):
     def about_us(self):
         # set up C
         c.title = c.site_name + " - About_us"
-        return {'c': c, 'h': self.helper, 'project_url': '/'}
+        return {'c': c, 'h': self.helper, 'project_url': '/','url':self.url}
     #faq page wouldn't work without the tutorial list
     @action(renderer="templates/contents/faq.mako")
     def faq(self):
         # set up C
         c.title = c.site_name + " - Faq"
-        return {'c': c, 'h': self.helper, 'project_url': '/'}
+        return {'c': c, 'h': self.helper, 'project_url': '/','url':self.url}
 
     @action(renderer="templates/contents/our_data.mako")
     def our_data(self):
         # set up C
         c.title = c.site_name + " - Our Data"
-        return {'c': c, 'h': self.helper, 'project_url': '/'}
+        return {'c': c, 'h': self.helper, 'project_url': '/','url':self.url}
     #faq page wouldn't work without the data_publication
     @action(renderer="templates/contents/our_publications.mako")
     def our_publications(self):
         # set up C
         c.title = c.site_name + " - Our Publications"
-        return {'c': c, 'h': self.helper, 'project_url': '/'}
+        return {'c': c, 'h': self.helper, 'project_url': '/','url':self.url}
 
     @action(renderer="templates/contents/disclaimer.mako")
     def disclaimer(self):
         # set up C
         c.title = c.site_name + " - Disclaimer"
-        return {'c': c, 'h': self.helper, 'project_url': '/'}
+        return {'c': c, 'h': self.helper, 'project_url': '/','url':self.url}
 
     @action(renderer="templates/contents/privacy_policy.mako")
     def privacy_policy(self):
         # set up C
         c.title = c.site_name + " - Privacy Policy"
-        return {'c': c, 'h': self.helper, 'project_url': '/'}
+        return {'c': c, 'h': self.helper, 'project_url': '/','url':self.url}
