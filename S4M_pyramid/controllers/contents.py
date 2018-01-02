bbb4431b1fecffb1a17e904fa82db6fb99a1518f
WU Yan <4wuyan@gmail.com>
2018-01-02 14:59:24 +1100

    removes code duplication in contents.py
    Previously, all controllers in contents.py returned the same python dictionary with duplicated code.
    That's error-prone.
    Now the dictionary is saved into an instance variable of the ContentsController class.

--- S4M_pyramid/controllers/contents.py
+++ S4M_pyramid/controllers/contents.py
@@ -1,4 +1,6 @@
 from pyramid_handlers import action
+
+# c is used to emulate the "from pylons import tmpl_context as c" functionality from Pylons
 from S4M_pyramid.lib.empty_class import EmptyClass as c
 from S4M_pyramid.lib.base import BaseController
 from S4M_pyramid.config import *
@@ -6,46 +8,50 @@ import psycopg2
 import psycopg2.extras
 class ContentsController(BaseController):
 
-   
+    def __init__(self, request):
+        super().__init__(request)
+        self.dictionary_to_return = {'c': c, 'h': self.helper, 'project_url': '/','url':self.url}
 
     @action(renderer="templates/contents/contact_us.mako")
     def contact_us(self):
         # set up C
         c.title=c.site_name+" - Contact_us"
-        return {'c': c, 'h': self.helper, 'project_url': '/','url':self.url}
+        return self.dictionary_to_return
 
     @action(renderer="templates/contents/about_us.mako")
     def about_us(self):
         # set up C
         c.title = c.site_name + " - About_us"
-        return {'c': c, 'h': self.helper, 'project_url': '/','url':self.url}
+        return self.dictionary_to_return
+
     #faq page wouldn't work without the tutorial list
     @action(renderer="templates/contents/faq.mako")
     def faq(self):
         # set up C
         c.title = c.site_name + " - Faq"
-        return {'c': c, 'h': self.helper, 'project_url': '/','url':self.url}
+        return self.dictionary_to_return
 
     @action(renderer="templates/contents/our_data.mako")
     def our_data(self):
         # set up C
         c.title = c.site_name + " - Our Data"
-        return {'c': c, 'h': self.helper, 'project_url': '/','url':self.url}
+        return self.dictionary_to_return
+
     #faq page wouldn't work without the data_publication
     @action(renderer="templates/contents/our_publications.mako")
     def our_publications(self):
         # set up C
         c.title = c.site_name + " - Our Publications"
-        return {'c': c, 'h': self.helper, 'project_url': '/','url':self.url}
+        return self.dictionary_to_return
 
     @action(renderer="templates/contents/disclaimer.mako")
     def disclaimer(self):
         # set up C
         c.title = c.site_name + " - Disclaimer"
-        return {'c': c, 'h': self.helper, 'project_url': '/','url':self.url}
+        return self.dictionary_to_return
 
     @action(renderer="templates/contents/privacy_policy.mako")
     def privacy_policy(self):
         # set up C
         c.title = c.site_name + " - Privacy Policy"
-        return {'c': c, 'h': self.helper, 'project_url': '/','url':self.url}
+        return self.dictionary_to_return
