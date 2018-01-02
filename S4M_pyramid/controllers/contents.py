94bd335837c3528d81411ea90e9b5fab95a86094
WU Yan <4wuyan@gmail.com>
2018-01-02 15:28:19 +1100

    moved the declaration of the shared dictionary to BaseController
    Since that dictionary is universal, i.e. not only for ContentsController but also for others, it's better to put the declaration of the universal dictionary in BaseController, i.e. lib/base.py

--- S4M_pyramid/controllers/contents.py
+++ S4M_pyramid/controllers/contents.py
@@ -8,50 +8,47 @@ import psycopg2
 import psycopg2.extras
 class ContentsController(BaseController):
 
-    def __init__(self, request):
-        super().__init__(request)
-        self.dictionary_to_return = {'c': c, 'h': self.helper, 'project_url': '/','url':self.url}
-
     @action(renderer="templates/contents/contact_us.mako")
     def contact_us(self):
         # set up C
         c.title=c.site_name+" - Contact_us"
-        return self.dictionary_to_return
+        return self.dictionary_returned_by_controllers
 
     @action(renderer="templates/contents/about_us.mako")
     def about_us(self):
         # set up C
         c.title = c.site_name + " - About_us"
-        return self.dictionary_to_return
+        return self.dictionary_returned_by_controllers
 
     #faq page wouldn't work without the tutorial list
     @action(renderer="templates/contents/faq.mako")
     def faq(self):
         # set up C
         c.title = c.site_name + " - Faq"
-        return self.dictionary_to_return
+        return self.dictionary_returned_by_controllers
 
     @action(renderer="templates/contents/our_data.mako")
     def our_data(self):
         # set up C
         c.title = c.site_name + " - Our Data"
-        return self.dictionary_to_return
+        return self.dictionary_returned_by_controllers
 
     #faq page wouldn't work without the data_publication
     @action(renderer="templates/contents/our_publications.mako")
     def our_publications(self):
         # set up C
         c.title = c.site_name + " - Our Publications"
-        return self.dictionary_to_return
+        return self.dictionary_returned_by_controllers
 
     @action(renderer="templates/contents/disclaimer.mako")
     def disclaimer(self):
         # set up C
         c.title = c.site_name + " - Disclaimer"
-        return self.dictionary_to_return
+        return self.dictionary_returned_by_controllers
 
     @action(renderer="templates/contents/privacy_policy.mako")
     def privacy_policy(self):
         # set up C
         c.title = c.site_name + " - Privacy Policy"
-        return self.dictionary_to_return
+        return self.dictionary_returned_by_controllers
+
