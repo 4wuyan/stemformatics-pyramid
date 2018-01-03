47cc2a9f61e2adc62c7907998fe92cafa43f1edc
WU Yan <4wuyan@gmail.com>
2018-01-03 16:34:02 +1100

    screenshot temp

---
+++ S4M_pyramid/controllers/auth.py
@@ -0,0 +1,45 @@
+#TODO-1
+import logging
+log = logging.getLogger(__name__)
+
+from S4M_pyramid.lib.base import BaseController
+    #from pylons import request, response, session, url, tmpl_context as c
+    #from pylons.controllers.util import abort, redirect
+# c is used to emulate the "from pylons import tmpl_context as c" functionality from Pylons
+from S4M_pyramid.lib.empty_class import EmptyClass as c
+
+# trying to find where db is set
+from S4M_pyramid.model.stemformatics import Stemformatics_Auth
+
+# Import the email modules we'll need
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+from S4M_pyramid.config import config
+
+from paste.deploy.converters import asbool
+
+# for some reason it is not applied in guide/lib/base.py
+import guide.lib.helpers as h
+
+from datetime import datetime, timedelta
+
+CAPTCHA_ENABLED = asbool(config['captcha.enabled'])
+
+import hashlib
+
+
+class AuthController(BaseController):
+
+    def __before__(self):
+
+        super(AuthController, self).__before__ ()
+
+        # clear out expired users first
+        Stemformatics_Auth.clear_expired_unconfirmed_users(db)
+
+        # clear out expired password resets too
+        Stemformatics_Auth.clear_expired_password_resets(db)
+        c.guest_username = config['guest_username']
+
+# heading off to Stemformatics_Auth
