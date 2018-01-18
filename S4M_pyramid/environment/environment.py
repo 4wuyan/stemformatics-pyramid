e6ed6cbd62cf0e05e3f56cf9ad47bcc97ab9743f
jameXPS13PyCharm <wjcauskew@hotmail.com>
2018-01-18 16:37:20 +1100

    fixed a typo

--- S4M_pyramid/environment/environment.py
+++ S4M_pyramid/environment/environment.py
@@ -1,4 +1,4 @@
 import S4M_pyramid.lib.app_globals as app_globals
 from S4M_pyramid.config import config
 def load_environment():
-    config["app_global"] = app_globals.Globals()
+    config["app_globals"] = app_globals.Globals()
