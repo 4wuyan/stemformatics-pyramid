05aa7e92fe5129e4b4f5f2a3cc9c9d117f49b9ae
WU Yan <4wuyan@gmail.com>
2018-01-27 23:27:16 +1100

    now we can access Stemformatics_?.. model class directly from model.stemformatics. And db is also put in this package.

--- S4M_pyramid/model/stemformatics/__init__.py
+++ S4M_pyramid/model/stemformatics/__init__.py
@@ -8,43 +8,29 @@ import logging
 
 log = logging.getLogger(__name__)
 
+from .stemformatics_dataset import Stemformatics_Dataset
+from .stemformatics_expression import Stemformatics_Expression
+from .stemformatics_gene import Stemformatics_Gene
+from .stemformatics_audit import Stemformatics_Audit
+from .stemformatics_auth import Stemformatics_Auth
+from .stemformatics_admin import Stemformatics_Admin
+from .stemformatics_probe import Stemformatics_Probe
+from .stemformatics_gene_set import Stemformatics_Gene_Set
+from .stemformatics_notification import Stemformatics_Notification
+from .stemformatics_help import Stemformatics_Help
+# from .stemformatics_export import *
+# from .stemformatics_msc_signature import *
+# from .stemformatics_transcript import *
+# from .stemformatics_job import *
+# from .stemformatics_shared_resource import *
+# from .stemformatics_ensembl_upgrade import *
+
 import zlib
 import json
 
-# we are trying to avoid using "import *" in the new pyramid code,
-# therefore __all__ might also need to be avoided
-#__all__ = [
-
-    # From imported modules ...
-    #'Stemformatics_Dataset',
-    #'Stemformatics_Audit',
-    #'Stemformatics_Gene',
-    #'Stemformatics_Transcript',
-    #'Stemformatics_Expression',
-    #'Stemformatics_Probe',
-    #'Stemformatics_Admin',
-    #'Stemformatics_Auth',
-    #'Stemformatics_Export',
-    #'Stemformatics_Job',
-    #'Stemformatics_Shared_Resource',
-    #'Stemformatics_Ensembl_Upgrade',
-    #'Stemformatics_Notification',
-    #'Stemformatics_Msc_Signature',
-    #'Stemformatics_Gene_Set',
-    #'Stemformatics_Help'
-#]
-
-
-# ------------------------------------------------------------------------------
-# In pylons,this is used to setup the db variable for ORM use, we do the setup
-# at the controller level to make the code more readable in pyramid,therefore this
-# method is deprecated
-# ------------------------------------------------------------------------------
-
-#db = None
-#engine = None
-
-def init_model(db_engine):
-    pass
-
+from sqlalchemy import create_engine
+import sqlsoup
+from S4M_pyramid.config import config
+engine = create_engine(config['orm_conn_string'])
+db_deprecated_pylons_orm = sqlsoup.SQLSoup(engine)
 
