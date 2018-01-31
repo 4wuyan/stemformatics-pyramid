afff227b8dda449c720e30ebd692ec25343a0d89
jameXPS13PyCharm <wjcauskew@hotmail.com>
2018-01-31 15:06:24 +1100

    added more decode in dataset model

--- S4M_pyramid/model/stemformatics/stemformatics_dataset.py
+++ S4M_pyramid/model/stemformatics/stemformatics_dataset.py
@@ -474,6 +474,8 @@ All functions have a try that will return None if errors are found
             r_server = redis.Redis(unix_socket_path=config['redis_server'])
             label_name = 'choose_dataset_details'
             result = r_server.get(label_name)
+            if result is not None:
+                result = result.decode('utf-8')
             temp_datasets = json.loads(result)
 
             for ds_id in ds_ids:
@@ -1159,11 +1161,11 @@ All functions have a try that will return None if errors are found
         delimiter = config['redis_delimiter']
         try:
             label_name = "user_dataset_availability"+delimiter+str(uid)
-            dict_uid = json.loads(r_server.get(label_name))
+            dict_uid = json.loads(r_server.get(label_name).decode('utf-8'))
         except:
             uid = 0 #default guest account
             label_name = "user_dataset_availability"+delimiter+str(uid)
-            dict_uid = json.loads(r_server.get(label_name))
+            dict_uid = json.loads(r_server.get(label_name).decode('utf-8'))
 
         return dict_uid
 
