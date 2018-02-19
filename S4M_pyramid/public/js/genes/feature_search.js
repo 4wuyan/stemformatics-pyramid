e02f6d0c06fafe3dc998c1598db47fb2741323bc
WU Yan <4wuyan@gmail.com>
2018-02-19 18:12:58 +1100

    update public/js/genes/feature_search.js

--- S4M_pyramid/public/js/genes/feature_search.js
+++ S4M_pyramid/public/js/genes/feature_search.js
@@ -7,19 +7,19 @@ var graphHeight = 275;
 
 $(document).ready(function() {
 
-    // error if more than X found, only displayX 
+    // error if more than X found, only displayX
     var error_div = $('#error_message');
-    var error_message = error_div.html().length; 
+    var error_message = error_div.html().length;
     if (error_message > 0) {
         error_div.removeClass('hidden');
     }
 
-    $('#exportTableCSVButton').live('click',function(){
-        $('#downloadGeneSearch').table2CSV();	
+    $('body').on('click','exportTableCSVButton',function(){
+        $('#downloadGeneSearch').table2CSV();
     });
-    
-    
-    
+
+
+
     $('#searchGenes').dataTable( {
         "bPaginate": false,
         "bLengthChange": false,
@@ -28,45 +28,44 @@ $(document).ready(function() {
         "bInfo": false,
         "aaSorting": [[ 1, "asc" ]],
         "bAutoWidth": false } );
-    
-    
+
+
 
     // **************** All the auto complete functions **************************
-    var details = new Object();    
+    var details = new Object();
     details.id = "#featureSearch";
     details.data_url = BASE_PATH + 'genes/get_feature_search_autocomplete?feature_type=all&db_id=';
     details.target_url = BASE_PATH + 'genes/feature_search?feature_search_term=';
     details.append_to = ".searchBox";
     setup_feature_search_autocomplete(details);
 
-    
+
     // **************** Cancel Form submit **************************
-    
-    
-   
+
+
+
     // each time the enter key pressed, get the gene details
-    $('form').submit(function(){ 
-        
-        
+    $('form').submit(function(){
+
+
         $("#featureSearch").autocomplete('close');
-        
+
         // getGeneDetails();
         window.location = BASE_PATH + 'genes/feature_search?feature_search_term=' + $("#featureSearch").val();
-        
-        return false; 
+
+        return false;
     });
-    
-    
+
+
     /* *************** Initial actions ******************** */
-    
+
     // choose Gene Selection first
-    
+
     // have to fix firefox issue with focus not highlighting all text
     // Set the focus up on gene search straight away
     $('#featureSearch').focus();
-    
-   
-    
-   
-});
 
+
+
+
+});
