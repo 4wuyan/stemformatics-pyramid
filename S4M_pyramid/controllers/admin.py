a532d22b7a9d7bd889a7dd4960bc27d00a77d405
isha <isha.nagpal@unimelb.edu.au>
2018-03-12 13:57:00 +1100

    T#2621 - changes for url param

--- S4M_pyramid/controllers/admin.py
+++ S4M_pyramid/controllers/admin.py
@@ -9,7 +9,7 @@ from pyramid_handlers import action
 from S4M_pyramid.lib.deprecated_pylons_globals import magic_globals, url, app_globals as g, config
 from S4M_pyramid.model.stemformatics import db_deprecated_pylons_orm as db
 from S4M_pyramid.lib.base import BaseController
-from S4M_pyramid.model.stemformatics import Stemformatics_Auth, Stemformatics_Dataset
+from S4M_pyramid.model.stemformatics import Stemformatics_Auth, Stemformatics_Dataset, Stemformatics_Expression
 
 
 # Import smtplib for the actual sending function
@@ -182,9 +182,9 @@ class AdminController(BaseController):
     ''' This is to update the cumulative files '''
     @Stemformatics_Auth.authorise(db)
     @action(renderer="string")
-    def setup_redis_cumulative(self,id):
+    def setup_redis_cumulative(self):
         try:
-            ds_id = int(id)
+            ds_id = self.request.matchdict['id']
         except:
             return "Error with this. Must be an integer"
 
